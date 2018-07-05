aadce24cfc05e47c9aec8d000504d724ebcd6627
LittleFall <30543181+LittleFall@users.noreply.github.com>
2018-07-05 21:05:32 +0800

    Create comp.bat

---
+++ Assist/comp.bat
@@ -0,0 +1,10 @@
+:: 在存在rand.exe的情况下,对my.exe和std.exe进行对拍
+@echo off  
+:loop  
+    rand.exe %random% > data.in
+    std.exe < data.in > std.out
+    my.exe < data.in > my.out
+    fc my.out std.out 
+if not errorlevel 1 goto loop  
+pause
+goto loop
